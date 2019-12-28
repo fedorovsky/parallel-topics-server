45780989a56df09226bcef1564e12906d0a177e6
Anton Fedorovskyi <anton.fedorovsky@gmail.com>
2019-12-28 10:26:10 +0200

    init commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const logger = require("morgan");
+const cors = require("cors");
+
+const app = express();
+app.use(logger("dev"));
+app.use(cors());
+app.use(bodyParser.json());
+
+app.get("/", (req, res) => {
+  console.log(req.body);
+  res.json({
+    name: "test response"
+  });
+});
+
+const PORT = 5000;
+app.listen(PORT, () => {
+  console.log(`Server was started on port: ${PORT}`);
+});
